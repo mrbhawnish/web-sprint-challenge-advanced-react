0c3b49947bd9997732c6053067cd7ac787ef0cbe
bhawnishk1 <mrbhawnish@gmail.com>
2020-10-02 17:28:51 -0700

    tests and MVP completed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+afterEach(cleanup)
+test("form header renders", () => { 
+    //Arrange
+    const { getByTestId } = render(<CheckoutForm />)
 
-test("form header renders", () => {});
+     //Act and Assert
+    const formHeader = getByTestId(/formHeader/i)
+    expect(formHeader).toHaveTextContent(/Checkout Form/i)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    //Arranging
+    const { getByLabelText, getByTestId, queryByTestId} = render(<CheckoutForm />)
+    const input = getByLabelText(/First Name:/i)
+    const submitButton = getByTestId(/submitButton/i)
+    
+    //Some actions
+    fireEvent.change(input, { target: { value: "something"}})
+    fireEvent.click(submitButton)
+    //Once the submit is clicked it renders our successMessage:
+    const successMessage = screen.getByTestId(/successMessage/i)
+    //Our Assertion for the expected test value
+    expect(successMessage).toHaveTextContent(/something/i)
+    
+});
